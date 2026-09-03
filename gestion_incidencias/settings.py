18c464637b7e79d662af048c43bf99c6f67bb278
Miguel Rolando Rimari Flores <miguelrolandorimariflores@gmail.com>
2026-09-03 13:59:18 -0500

    config: agrega STATIC_ROOT para despliegue

--- gestion_incidencias/settings.py
+++ gestion_incidencias/settings.py
@@ -122,9 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGIN_URL = 'incidencias:login'
 LOGIN_REDIRECT_URL = 'incidencias:dashboard'
 LOGOUT_REDIRECT_URL = 'incidencias:login'
+
